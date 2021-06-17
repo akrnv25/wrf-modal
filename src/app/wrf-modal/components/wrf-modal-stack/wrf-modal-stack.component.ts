a92e3c3e8bc67f1dbe821776d2d5e7f47f3b68a4
korenev6052 <korenev6052@gmail.com>
2021-06-17 14:11:10 +0300

    renamed stack component

---
+++ src/app/wrf-modal/components/wrf-modal-stack/wrf-modal-stack.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wrf-modal-stack',
+  templateUrl: './wrf-modal-stack.component.html',
+  styleUrls: ['./wrf-modal-stack.component.scss']
+})
+export class WrfModalStackComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/wrf-modal/wrf-modal.module.ts
+++ src/app/wrf-modal/wrf-modal.module.ts
@@ -3,18 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { WrfModalComponent } from './components/wrf-modal/wrf-modal.component';
 import { WrfModalControllerService } from './services/wrf-modal-controller.service';
-import { WrfModalsStackComponent } from './components/wrf-modals-stack/wrf-modals-stack.component';
+import { WrfModalStackComponent } from './components/wrf-modal-stack/wrf-modal-stack.component';
 
 @NgModule({
   declarations: [
     WrfModalComponent,
-    WrfModalsStackComponent
+    WrfModalStackComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    WrfModalsStackComponent
+    WrfModalStackComponent
   ],
   providers: [
     WrfModalControllerService
